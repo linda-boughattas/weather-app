52654c6622c58a98fdb8e79b44e916ddbd2e316f
Linda Boughattas <linda.boughattas@enicar.ucar.tn>
2024-07-27 00:25:54 +0200

    displaying weather images

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,7 @@ const apiURL ="https://api.openweathermap.org/data/2.5/weather?units=metric"
 
 const searchBar=document.getElementById("searchBar")
 const searchBtn=document.getElementById("searchButton")
+const weatherPic=document.querySelector(".weather-icon")
 
 async function checkWeather(city){
     const response = await fetch( apiURL +`&q=${city}` + `&appid=${apiKey}`)
@@ -14,8 +15,24 @@ async function checkWeather(city){
     document.querySelector(".city").innerHTML= data.name
     document.querySelector(".humidity__value").innerHTML= Math.round(data.main.humidity) + "%"
     document.querySelector(".wind__value").innerHTML= Math.round(data.wind.speed) + "km/h"
+
+    if (data.weather[0].main == 'Clear') {
+        weatherPic.src="./assets/img/clear.png"
+    }else if (data.weather[0].main == 'Clouds') {
+        weatherPic.src="./assets/img/clouds.png"
+    }else if (data.weather[0].main == 'Drizzle') {
+        weatherPic.src="./assets/img/drizzle.png"
+    }else if (data.weather[0].main == 'Mist') {
+        weatherPic.src="./assets/img/mist.png"
+    }else if (data.weather[0].main == 'Rain') {
+        weatherPic.src="./assets/img/rain.png"
+    }else {
+        weatherPic.src="./assets/img/snow.png"
+    }
+
+    document.querySelector(".weather").style.display="block"
+    document.querySelector(".details").style.display="block"
 }
-checkWeather("monastir")
 
 searchBtn.addEventListener("click", ()=>{
     checkWeather(searchBar.value)
